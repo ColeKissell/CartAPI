3840c25eafa0446317b399b23d743549f19a0cb9
Paul Barrett <paulbarrett@Pauls-MacBook-Air.local>
2018-10-19 09:14:57 -0600

    cleaned up email validation code

--- models/users.js
+++ models/users.js
@@ -1,22 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+require ('mongoose-type-email')
 
-const validateEmail = function(Email) {
-    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(Email)
-};
 
 const UsersSchema = new Schema ({
     Role: {type:  [String] , required: true},
 	Username:{type: String, required: true},
-	Email: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: 'Email address is required',
-        validate: [validateEmail, 'Please fill a valid email address'],
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-    },
+	Email: {type: mongoose.SchemaTypes.Email, required: true},
     FirstName: String,
     LastName: String,
 	Password: {type: String, required: true},
